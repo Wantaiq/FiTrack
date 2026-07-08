f390fb6536dc729dbcc665e84a7b6e97957eda9e
wantaiq <99611999+Wantaiq@users.noreply.github.com>
2026-07-08 15:38:28 +0200

    feat: import auth module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
+  imports: [ConfigModule, DatabaseModule, AuthModule],
   controllers: [],
   providers: [],
 })
