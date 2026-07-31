46a4070d8192dac6900d5f7cf122c808f1509a81
wantaiq <99611999+Wantaiq@users.noreply.github.com>
2026-07-31 18:23:46 +0200

    feat: style details page

--- frontend/src/features/exercises/components/ExerciseDetails.tsx
+++ frontend/src/features/exercises/components/ExerciseDetails.tsx
@@ -1,4 +1,15 @@
+import {
+  Badge,
+  Card,
+  Heading,
+  IconButton,
+  Stack,
+  StackSeparator,
+  Text,
+} from '@chakra-ui/react';
 import type { ExerciseFull } from '../schemas/exercise.schema';
+import { Link } from 'react-router';
+import { RxChevronLeft } from 'react-icons/rx';
 
 type Props = {
   exercise: ExerciseFull;
@@ -6,21 +17,80 @@ type Props = {
 
 function ExerciseDetails({ exercise }: Props) {
   return (
-    <>
-      <h1>{exercise.name}</h1>
-      <p>{exercise.description}</p>
-      <p>{exercise.difficulty}</p>
-      <p>{exercise.mechanic}</p>
-      <p>{exercise.type}</p>
-      <ul>
-        {exercise.instructions.map((instruction) => (
-          <li key={instruction.id}>
-            <p>{instruction.title}</p>
-            <p>{instruction.text}</p>
-          </li>
-        ))}
-      </ul>
-    </>
+    <Stack gap={8}>
+      <Stack direction="row" alignItems="center" gap={4}>
+        <IconButton size={'md'} aria-label="Go back" variant={'subtle'} asChild>
+          <Link to="/exercises">
+            <RxChevronLeft />
+          </Link>
+        </IconButton>
+        <Stack alignItems={'start'}>
+          <Heading as="h1" fontSize="2xl">
+            {exercise.name}
+          </Heading>
+          <Stack direction="row" alignItems="center" gap={2}>
+            <Badge colorPalette="cyan" size="sm">
+              {exercise.difficulty}
+            </Badge>
+            <Badge colorPalette="cyan" size="sm">
+              {exercise.type}
+            </Badge>
+            <Badge colorPalette="cyan" size="sm">
+              {exercise.mechanic}
+            </Badge>
+          </Stack>
+        </Stack>
+      </Stack>
+      <Card.Root w="full">
+        <Card.Header
+          borderBottomWidth={1}
+          paddingBlockEnd={4}
+          borderColor={'gray.muted'}
+        >
+          <Card.Title fontSize={'xl'}>Description</Card.Title>
+        </Card.Header>
+        <Card.Body>{exercise.description}</Card.Body>
+      </Card.Root>
+      <Card.Root w="full">
+        <Card.Header
+          borderBottomWidth={1}
+          paddingBlockEnd={4}
+          borderColor={'gray.muted'}
+        >
+          <Card.Title as={'h2'} fontSize={'xl'}>
+            Instructions
+          </Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Stack as={'ul'} gap={4} separator={<StackSeparator />}>
+            {exercise.instructions.map((instruction, idx) => (
+              <Stack
+                direction="row"
+                key={instruction.id}
+                as={'li'}
+                alignItems={'start'}
+                gap={4}
+              >
+                <Badge
+                  colorPalette={'brand'}
+                  size={'lg'}
+                  variant={'solid'}
+                  fontWeight={'semibold'}
+                >
+                  {idx + 1}
+                </Badge>
+                <Stack gap={0}>
+                  <Heading as={'h3'} fontSize={'lg'}>
+                    {instruction.title}
+                  </Heading>
+                  <Text>{instruction.text}</Text>
+                </Stack>
+              </Stack>
+            ))}
+          </Stack>
+        </Card.Body>
+      </Card.Root>
+    </Stack>
   );
 }
 
