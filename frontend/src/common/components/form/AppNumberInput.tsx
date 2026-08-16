a62bcaefc787d737a640a629b95087dbf22b83c0
wantaiq <99611999+Wantaiq@users.noreply.github.com>
2026-08-16 19:46:55 +0200

    feat: add number input

---
+++ frontend/src/common/components/form/AppNumberInput.tsx
@@ -0,0 +1,64 @@
+import { Field, NumberInput } from '@chakra-ui/react';
+import { useId } from 'react';
+import {
+  Controller,
+  useFormContext,
+  type FieldValues,
+  type Path,
+} from 'react-hook-form';
+
+type AppNumberInputProps<T extends FieldValues> = {
+  name: Path<T>;
+  label: string;
+  required?: boolean;
+  placeholder?: string;
+};
+
+function AppNumberInput<T extends FieldValues>({
+  name,
+  label,
+  required = true,
+}: AppNumberInputProps<T>) {
+  const id = useId();
+  const { getFieldState, formState, control } = useFormContext();
+
+  const { error } = getFieldState(name, formState);
+
+  return (
+    <Field.Root invalid={!!error} required={required} gap="2">
+      <Field.Label htmlFor={id}>
+        {label}
+        <Field.RequiredIndicator />
+      </Field.Label>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field }) => (
+          <NumberInput.Root
+            w="full"
+            disabled={field.disabled}
+            name={field.name}
+            value={field.value}
+            onValueChange={({ value }) => {
+              field.onChange(value);
+            }}
+          >
+            <NumberInput.Control />
+            <NumberInput.Input onBlur={field.onBlur} />
+          </NumberInput.Root>
+        )}
+      />
+      <Field.ErrorText
+        fontWeight="semibold"
+        as="div"
+        bg="bg.error"
+        p="2"
+        w="full"
+      >
+        {error?.message}
+      </Field.ErrorText>
+    </Field.Root>
+  );
+}
+
+export default AppNumberInput;
--- frontend/src/common/components/form/index.ts
+++ frontend/src/common/components/form/index.ts
@@ -3,3 +3,4 @@ export { default as AppInput } from './AppInput';
 export { default as AppTextarea } from './AppTextarea';
 export { default as AppFieldArray } from './AppFieldArray';
 export { default as AppSelect } from './AppSelect';
+export { default as AppNumberInput } from './AppNumberInput';
