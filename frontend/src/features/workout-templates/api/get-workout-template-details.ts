955ba457335874ff3761b41c65f54b3f97a8cdc3
wantaiq <99611999+Wantaiq@users.noreply.github.com>
2026-08-20 15:15:53 +0200

    fix: add types

--- frontend/src/features/workout-templates/api/get-workout-template-details.ts
+++ frontend/src/features/workout-templates/api/get-workout-template-details.ts
@@ -1,6 +1,9 @@
 import client from '@/common/api/client';
+import type { WorkoutTemplateFull } from '../schemas/workout-template.schema';
 
-async function getWorkoutTemplateDetails(id: string) {
+async function getWorkoutTemplateDetails(
+  id: string,
+): Promise<WorkoutTemplateFull> {
   const { data } = await client.get(`/workout-templates/${id}`);
 
   return data;
